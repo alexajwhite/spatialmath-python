84881777fe8f90ed4af6bb19495edbdbd333ed54
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-03 07:16:25 +0000

    Deploy petercorke/spatialmath-python to github.com/petercorke/spatialmath-python.git:gh-pages

--- spatialmath/__init__.py
+++
@@ -1,1 +0,0 @@
-from spatialmath.pose2d import *from spatialmath.pose3d import *from spatialmath.quaternion import *
--- spatialmath/base/__init__.py
+++
@@ -1,5 +0,0 @@
-from spatialmath.base.transforms2d import *
-from spatialmath.base.transforms3d import *
-from spatialmath.base.transformsNd import *
-from spatialmath.base.vectors import *
-from spatialmath.base.quaternions import *
